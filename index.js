577cb1eef10e18f1c6b2e936db0635e7305841f6
xcodder <arthur.3@protonmail.com>
2018-04-10 17:08:54 +0300

    added must, ambigious

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function genStr(options) {
+    function shuffleString(str) {
+      var a = str.split(""),
+          n = a.length;
+  
+      for(var i = n - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var tmp = a[i];
+          a[i] = a[j];
+          a[j] = tmp;
+      }
+      return a.join("");
+  }
+  function getRandomFromArray(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
   let length = options.length,
-      inc = options.include
+      inc = options.include,
+      must = options.must,
+      ambigious = options.ambigious
 
   let lower = "abcdefghjkmnopqrstuvwxyz"
   let upper = lower.toUpperCase()
   lower += 'i'
   upper += 'L'
+  if(ambigious) {
+    lower += 'I'
+    upper += 'l'
+  }
   let special = "-_!@#$%^&*()"
   let numbers = "0123456789"
 
@@ -27,8 +48,23 @@ function genStr(options) {
   let chars = Object.values(all).join('')
   let charsLength = chars.length
   let result = ""
-  for(let i = 0; i < length; i++) {
-    result += chars[~~(Math.floor(Math.random() * charsLength))]
+  if(must) {
+    for(let field in must) {
+      let length = must[field]
+      for(let i = 0; i < length; i++) {
+        result += getRandomFromArray(all[field])
+      }
+    }
+  }
+  let remainingLength = length - result.length
+  for(let i = 0; i < remainingLength; i++) {
+    result += getRandomFromArray(chars)
+  }
+  if(must) {
+    result = shuffleString(result)
+  }
+  if(result.length > length) {
+    throw new Error("Length is less than summed must fields' values")
   }
   return result
 }
